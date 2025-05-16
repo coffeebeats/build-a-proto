7d730aa88a861e1f4ae80c0e4f801d277db6f310
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-05-16 10:44:10 -0700

    feat: add `compile` command to the CLI

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+mod cmd;
+mod parser;
+
 use anyhow::Result;
 use clap::Parser;
+use cmd::Commands;
 
 #[derive(Parser)]
 #[command(name = "baproto", author, version, about)]
 #[command(arg_required_else_help = true)]
 struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+
     /// Silences all non-essential logging.
     #[arg(short, long, global = true, conflicts_with = "verbose")]
     quiet: bool,
@@ -15,6 +22,10 @@ struct Cli {
 }
 
 fn main() -> Result<()> {
-    let _ = Cli::parse();
-    Ok(())
+    let cli = Cli::parse();
+
+    match cli.command {
+        /* ------------------------ Category: Compile ----------------------- */
+        Commands::Compile(args) => cmd::compile::handle(args),
+    }
 }
