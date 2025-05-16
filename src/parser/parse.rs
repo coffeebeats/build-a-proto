39dc8f852128440af561d8cb381c52c221082585
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-05-16 14:42:09 -0700

    wip

--- src/parser/parse.rs
+++ src/parser/parse.rs
@@ -1,3 +1,31 @@
-pub fn parse(_input: &str) -> anyhow::Result<()> {
+use chumsky::prelude::*;
+
+#[derive(Debug)]
+enum Expr<'a> {
+    Num(f64),
+    Var(&'a str),
+
+    Neg(Box<Expr<'a>>),
+    Add(Box<Expr<'a>>, Box<Expr<'a>>),
+    Sub(Box<Expr<'a>>, Box<Expr<'a>>),
+    Mul(Box<Expr<'a>>, Box<Expr<'a>>),
+    Div(Box<Expr<'a>>, Box<Expr<'a>>),
+
+    Call(&'a str, Vec<Expr<'a>>),
+    Let {
+        name: &'a str,
+        rhs: Box<Expr<'a>>,
+        then: Box<Expr<'a>>,
+    },
+    Fn {
+        name: &'a str,
+        args: Vec<&'a str>,
+        body: Box<Expr<'a>>,
+        then: Box<Expr<'a>>,
+    },
+}
+
+pub fn parse(input: &str) -> anyhow::Result<()> {
+    println!("{}", input);
     Ok(())
 }
