f693906094d02e49421aba8103ede66b45bc3612
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-05-21 23:25:10 -0700

    fix(cmd): utilize new parser in `compile`, print errors

--- src/cmd/compile.rs
+++ src/cmd/compile.rs
@@ -1,8 +1,14 @@
 use anyhow::anyhow;
+use ariadne::Color;
+use ariadne::Label;
+use ariadne::Report;
+use ariadne::ReportKind;
+use ariadne::sources;
 use std::path::Path;
 use std::path::PathBuf;
 
 use crate::parse::lex;
+use crate::parse::parse;
 
 /* -------------------------------------------------------------------------- */
 /*                                Struct: Args                                */
@@ -44,9 +50,7 @@ pub struct Bindings {
 pub fn handle(args: Args) -> anyhow::Result<()> {
     let out_dir = parse_out_dir(args.out)?;
 
-    let mut failed: Vec<(PathBuf, Vec<_>)> = vec![];
-
-    for path in args.files {
+    Ok(for path in args.files {
         println!(
             "Compiling {:?} into {:?} (binding={})",
             path,
@@ -56,34 +60,61 @@ pub fn handle(args: Args) -> anyhow::Result<()> {
 
         let contents = std::fs::read_to_string(&path).map_err(|e| anyhow!(e))?;
 
-        match lex(&contents) {
-            Err(errs) => {
-                failed.push((path, errs.into_iter().map(|e| e.into_owned()).collect()));
+        let path = path
+            .canonicalize()
+            .map_err(|e| anyhow!(e))?
+            .to_owned()
+            .into_os_string()
+            .into_string()
+            .map_err(|s| anyhow!("couldn't convert path to string: {:?}", s))?;
+
+        let mut parse_errs = vec![];
+
+        let (tokens, lex_errs) = lex(&contents);
+
+        if let Some(tokens) = tokens.as_ref() {
+            let (exprs, errs) = parse(tokens, contents.len());
+            parse_errs = errs;
+
+            if let Some(exprs) = exprs.as_ref() {
+                for expr in exprs {
+                    println!("\n{:?}", expr);
+                }
             }
-            Ok(_) => {
-                todo!()
-            }
-        };
-    }
-
-    if failed.is_empty() {
-        return Ok(());
-    }
-
-    for failure in failed {
-        println!("Failed to parse file: {:?}", failure.0);
-
-        for err in failure.1 {
-            println!("  {:?}", err);
         }
-    }
 
-    Err(anyhow!("Compilation failed"))
+        println!("\n");
+
+        lex_errs
+            .into_iter()
+            .map(|e| e.map_token(|c| c.to_string()))
+            .chain(
+                parse_errs
+                    .into_iter()
+                    .map(|e| e.map_token(|t| t.to_string())),
+            )
+            .for_each(|e| {
+                Report::build(ReportKind::Error, (path.to_owned(), e.span().into_range()))
+                    .with_config(ariadne::Config::new().with_index_type(ariadne::IndexType::Byte))
+                    .with_message(e.to_string())
+                    .with_label(
+                        Label::new((path.to_owned(), e.span().into_range()))
+                            .with_message(e.reason().to_string())
+                            .with_color(Color::Red),
+                    )
+                    .with_labels(e.contexts().map(|(label, span)| {
+                        Label::new((path.to_owned(), span.into_range()))
+                            .with_message(format!("while parsing this {}", label))
+                            .with_color(Color::Yellow)
+                    }))
+                    .finish()
+                    .print(sources([(path.to_owned(), contents.clone())]))
+                    .unwrap()
+            });
+    })
 }
 
-/* -------------------------------------------------------------------------- */
-/*                           Function: parse_out_dir                          */
-/* -------------------------------------------------------------------------- */
+/* ---------------------------- Fn: parse_out_dir --------------------------- */
 
 /// `parse_out_dir` accepts an optional output directory and returns the
 /// directory in which generated artifacts should be written.
