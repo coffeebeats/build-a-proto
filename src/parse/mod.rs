e1f5efe713e9bfc7473cb92b37a28b25aff0d828
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-05-21 23:24:39 -0700

    feat(parse): implement a parser which emits `Expr`s

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,6 +1,12 @@
 mod lexer;
+mod parser;
 
 /* ------------------------------- Mod: Lexer ------------------------------- */
 
 #[allow(unused_imports)]
 pub use lexer::*;
+
+/* ------------------------------- Mod: Parser ------------------------------ */
+
+#[allow(unused_imports)]
+pub use parser::*;
