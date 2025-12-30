25cf3e11aa43e5db21374fd8aa5247b56a481de1
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-12-29 21:24:39 -0600

    chore(ast,core): migrate `ast` package into `core`

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,6 +1,8 @@
 pub mod enumeration;
 pub mod message;
+pub mod package;
 pub mod path;
+pub mod reference;
 pub mod registry;
 pub mod types;
 
@@ -12,9 +14,17 @@ pub use enumeration::*;
 
 pub use message::*;
 
+/* ------------------------------ Mod: Package ------------------------------ */
+
+pub use package::*;
+
 /* -------------------------------- Mod: Path ------------------------------- */
 
 pub use path::*;
+
+/* ----------------------------- Mod: Reference ----------------------------- */
+
+pub use reference::*;
 
 /* ------------------------------ Mod: Registry ----------------------------- */
 
@@ -32,22 +42,20 @@ use derive_builder::Builder;
 use derive_more::Display;
 use std::path::PathBuf;
 
-use crate::syntax::PackageName;
-
 #[derive(Builder, Clone, Debug, Display)]
 #[display("Module({:?}): {:?}", self.path.as_path(), self.messages.iter().chain(self.enums.iter()).map(ToString::to_string).collect::<Vec<_>>())]
 pub struct Module {
-    pub path: PathBuf,
-    pub package: PackageName,
     #[builder(default)]
     pub deps: Vec<SchemaImport>,
     #[builder(default)]
     pub enums: Vec<Descriptor>,
     #[builder(default)]
     pub messages: Vec<Descriptor>,
+    pub path: PathBuf,
+    pub package: PackageName,
 }
 
-/* -------------------------------- Impl: New ------------------------------- */
+/* ------------------------------ Impl: Module ------------------------------ */
 
 impl Module {
     pub fn new(path: PathBuf) -> Module {
