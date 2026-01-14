6b7886f0960fc38a20b8bfe48d6bc2990fc42f7c
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2026-01-13 18:03:51 -0800

    chore(compile): remove legacy compiler implementation

--- src/compile/mod.rs
+++ src/compile/mod.rs
@@ -1,27 +1,3 @@
-mod compiler;
-mod linker;
-mod prepare;
-mod register;
-mod symbol;
-
-/* ------------------------------ Mod: Compiler ----------------------------- */
-
-#[allow(dead_code)]
-pub use compiler::*;
-
-/* ------------------------------- Mod: Linker ------------------------------ */
-
-pub use linker::*;
-
-/* ------------------------------ Mod: Prepare ------------------------------ */
-
-pub use prepare::*;
-
-/* ------------------------------ Mod: Register ----------------------------- */
-
-#[allow(unused_imports)]
-pub use register::*;
-
 /* ------------------------------- Mod: Source ------------------------------ */
 
 mod source;
@@ -29,5 +5,5 @@ pub use source::*;
 
 /* ------------------------------- Mod: Symbol ------------------------------ */
 
-#[allow(unused_imports)]
+mod symbol;
 pub use symbol::*;
